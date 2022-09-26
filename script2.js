f6a533dd7fd925af7c033083846a65b813d6e269
JStaRFilms <johnoke2005@gmail.com>
2022-09-26 17:03:50 +0100

    change to js

---
+++ script2.js
@@ -0,0 +1,18 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
+
+var todosLenght = todos.length;
+// for (var i=0; i < todosLenght; i++) {
+//     todos.pop();
+// }
+
+// var counterOne = 10;
+// while (counterOne > 0) {
+//     console.log(counterOne);
+//     counterOne--
+// }
